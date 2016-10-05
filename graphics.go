eab0d3a9ccbb098b313f199e75b3c8e62329a70b
Dave Sizer <dave@sizetron.net>
2016-10-05 15:02:40 -0400

    Finished line drawing edge cases and started Bresenham

--- graphics.go
+++ graphics.go
@@ -25,7 +25,7 @@ type Drawable interface {
 
 // RGBColor is an RGB color with 8bit channels
 type RGBColor struct {
-	r, g, b byte
+	R, G, B byte
 }
 
 /* SoftFrameBuffer */
@@ -34,6 +34,7 @@ type RGBColor struct {
 type SoftFrameBuffer struct {
 	Width, Height int
 	buffer        [][]RGBColor
+	colors        map[RGBColor]string
 }
 
 // NewSoftFrameBuffer allocates a new SoftFrameBuffer with specified width and
@@ -41,8 +42,28 @@ type SoftFrameBuffer struct {
 func NewSoftFrameBuffer(width, height int) *SoftFrameBuffer {
 	newBuffer := &SoftFrameBuffer{
 		Width:  width,
-		Height: height,
-		buffer: make([][]RGBColor, height, width)}
+		Height: height}
+
+	newBuffer.colors = make(map[RGBColor]string)
+
+	white := RGBColor{255, 255, 255}
+	black := RGBColor{0, 0, 0}
+	// Hard code black and white for now
+	newBuffer.colors[white] = "#"
+	newBuffer.colors[black] = "*"
+
+	// Allocate the pixel buffer
+	newBuffer.buffer = make([][]RGBColor, newBuffer.Height+1)
+	for y := range newBuffer.buffer {
+		newBuffer.buffer[y] = make([]RGBColor, newBuffer.Width+1)
+	}
+
+	// Set the buffer to white
+	for y := 0; y < newBuffer.Height; y++ {
+		for x := 0; x < newBuffer.Width; x++ {
+			newBuffer.WritePixel(x, y, white)
+		}
+	}
 
 	return newBuffer
 }
@@ -52,7 +73,10 @@ func NewSoftFrameBuffer(width, height int) *SoftFrameBuffer {
 func (frameBuffer *SoftFrameBuffer) WritePixel(x, y int, color RGBColor) {
 
 	// We will reverse the y origin for compatibility with XPM output
-	frameBuffer.buffer[frameBuffer.Height-y][x] = color
+	frameBuffer.buffer[frameBuffer.Height-y-1][x] = color
+
+	// Add this to our set of colors
+	//frameBuffer.colors[color] = GenerateHexString(color)
 
 }
 
@@ -60,12 +84,12 @@ func (frameBuffer *SoftFrameBuffer) WritePixel(x, y int, color RGBColor) {
 
 // Scene is a virtual space that can be rendered to a frame buffer
 type Scene struct {
-	objects []Drawable
+	Objects []Drawable
 }
 
 // Render renders the scene to the given buffer
 func (scene *Scene) Render(buffer *SoftFrameBuffer) {
-	for _, object := range scene.objects {
+	for _, object := range scene.Objects {
 		object.Draw(buffer)
 	}
 }
@@ -102,6 +126,101 @@ func (line *Line) Draw(buffer *SoftFrameBuffer) {
 
 	line.clip(buffer)
 
+	if line.handleEdgeCases(buffer) {
+		return
+	}
+
+	// Unpack the line
+	qx, qy, rx, ry := line.Unpack()
+
+	var independentCoord, dependentCoord *int
+	var decider func() int
+
+	dx := rx - qx
+	dy := ry - qy
+
+	m := float64(dy / dx)
+
+	decisionConstantTerm := func() int {
+		return 2 * (rx*dy - ry*dx)
+	}
+
+	caseOneDecider := func() int {
+
+	}
+
+	// Use Bresenham's algorithm
+	// Case 1: 0 < m < 1
+	// Case 2: m > 1
+	// Case 3: m < -1
+	// Case 4: -1 < m < 0
+	if m > 0 && m < 1 {
+
+	} else if m > 1 {
+
+	} else if m < -1 {
+
+	} else if m > -1 && m < 0 {
+
+	}
+
+}
+
+func (line *Line) Unpack() (qx, qy, rx, ry int) {
+	qx = int(line.a.attributes[0])
+	qy = int(line.a.attributes[1])
+	rx = int(line.b.attributes[0])
+	ry = int(line.b.attributes[1])
+
+	return
+}
+
+func (line *Line) handleEdgeCases(buffer *SoftFrameBuffer) bool {
+	var length int
+	var variable *int
+	handled := false
+	black := RGBColor{0, 0, 0}
+
+	qx, qy, rx, ry := line.Unpack()
+
+	// Just a single point
+	if qx == rx && qy == ry {
+		buffer.WritePixel(qx, qy, black)
+		return true
+	}
+
+	if qy == ry { // Horizontal
+		// Check if we need to swap the points
+		if rx < qx {
+			swap(&rx, &qx)
+			swap(&ry, &qy)
+		}
+		variable = &qx
+		length = rx - qx
+		handled = true
+	} else if qx == rx { // Vertical
+		if ry < qy {
+			swap(&rx, &qx)
+			swap(&ry, &qy)
+		}
+		variable = &qy
+		length = ry - qy
+		handled = true
+	}
+
+	for i := 0; i < length; i++ {
+		buffer.WritePixel(qx, qy, black)
+		(*variable)++
+	}
+
+	return handled
+
+}
+
+func swap(a, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
 }
 
 /* PostScriptFile */
@@ -144,9 +263,9 @@ func OpenPostScriptFile(path string) (*PostScriptFile, error) {
 }
 
 // ParseObjects parses all Drawable geometry objects out of a PostScriptFile
-func (file *PostScriptFile) ParseObjects() ([]*Drawable, error) {
+func (file *PostScriptFile) ParseObjects() ([]Drawable, error) {
 
-	var objects []*Drawable
+	var objects []Drawable
 	// Check if the first line is the begin delimiter
 	scanner := file.scanner
 	scanner.Scan()
@@ -186,7 +305,7 @@ func (file *PostScriptFile) ParseObjects() ([]*Drawable, error) {
 				return objects, errors.New(errStr)
 			}
 
-			objects = append(objects, &object)
+			objects = append(objects, object)
 
 			/*
 				// Some line debug stuff
@@ -227,7 +346,7 @@ type XPMFile struct {
 }
 
 func OpenXPMFile(path string) (*XPMFile, error) {
-	fp, err := os.Open(path)
+	fp, err := os.Create(path)
 	if err != nil {
 		return nil, err
 	}
@@ -240,21 +359,49 @@ func OpenXPMFile(path string) (*XPMFile, error) {
 		writer:   writer}
 
 	file.XPMFileFormatStr =
-		"/* XPM */" +
-			"static char *sco100[] = {" +
-			"/* width height num_colors chars_per_pixel */" +
-			"\"%d %d %d %d\"" +
-			"/* colors */" +
+		"/* XPM */\n" +
+			"static char *sco100[] = {\n" +
+			"/* width height num_colors chars_per_pixel */\n" +
+			"\"%d %d %d %d\",\n" +
+			"/* colors */\n" +
 			"%s" +
-			"/* pixels */" +
+			"/* pixels */\n" +
 			"%s" +
-			"};"
+			"};\n"
 
 	return file, err
 }
 
-func (file *XPMFile) WriteSoftBuffer(buffer *SoftFrameBuffer) {
+// WriteSoftBuffer writes the given buffer to an xpm file
+func (file *XPMFile) WriteSoftBuffer(buffer *SoftFrameBuffer) error {
+	// Build the color list string
+	var colorList string
+	for k, v := range buffer.colors {
+		colorList += fmt.Sprintf("\"%s c #%s\",\n", v, GenerateHexString(k))
+	}
 
+	// Build the rows string
+	var rowsList string
+	for y := 0; y < buffer.Height; y++ {
+		rowsList += "\""
+		for x := 0; x < buffer.Width; x++ {
+			rowsList += buffer.colors[buffer.buffer[y][x]]
+		}
+		rowsList += "\""
+		if y != buffer.Height-1 {
+			rowsList += ","
+		}
+		rowsList += "\n"
+	}
+
+	// Put it all together
+	fileStr := fmt.Sprintf(file.XPMFileFormatStr, buffer.Width,
+		buffer.Height, len(buffer.colors), 1, colorList, rowsList)
+
+	_, err := file.writer.WriteString(fileStr)
+	file.writer.Flush()
+
+	return err
 }
 
 /* ---PostScript object parsing routines--- */
@@ -293,5 +440,5 @@ func getCwd() string {
 }
 
 func GenerateHexString(color RGBColor) string {
-	return ""
+	return fmt.Sprintf("%02x%02x%02x", color.R, color.G, color.B)
 }
